9fe7d9fdd186aff40562d6a36170861824274be3
A. M. M. Shahriar Kabir <ashahriarkabir@banglalink.net>
2025-12-16 14:22:03 +0600

    moving css designs to main.blade file as css is not working in hostinger

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,9 +1,9 @@
 <footer class="site-footer">
     <div class="container d-flex justify-content-center">
         <ul class="nav">
+            <li class="nav-item"><a class="nav-link" href="#">Blogs</a></li>
             <li class="nav-item"><a class="nav-link" href="#">Privacy Policy</a></li>
             <li class="nav-item"><a class="nav-link" href="#">Terms of Service</a></li>
-            <li class="nav-item"><a class="nav-link" href="#">About Us</a></li>
             <li class="nav-item"><a class="nav-link" href="#">Contact Us</a></li>
         </ul>
     </div>
